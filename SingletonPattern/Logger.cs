bae21112b7c5cf082dc96a1527a001bb1ae4d7d0
Reshmi Soman <resh.soma@gmail.com>
2025-02-17 20:43:23 +0530

    First Commit

---
+++ SingletonPattern/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace SingletonPattern
+{
+    public class Logger
+    {
+        private static Logger instance;
+        private Logger()
+        {
+
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Logger();
+                }
+                return instance;
+            }
+        }
+
+        //public static Logger GetInstance()
+        //{
+        //    if(logger == null)
+        //    {
+        //        logger = new Logger();
+        //    }
+        //    return logger;
+        //}
+    }
+}
---
+++ SingletonPattern/TestLogger.cs
@@ -0,0 +1,26 @@
+﻿namespace SingletonPattern
+{
+    class TestLogger
+    {
+        static void Main()
+        {
+            //singleton
+            Logger obj1 = Logger.Instance;
+            Logger obj2 = Logger.Instance;
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            //non singleton
+            Test obj3 = new Test();
+            Test obj4 = new Test();
+            Console.WriteLine(obj3.GetHashCode());
+            Console.WriteLine(obj4.GetHashCode());
+        }
+    }
+
+    class Test
+    {
+
+    }
+
+}
